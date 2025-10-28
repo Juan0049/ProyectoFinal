764b4c3a81469b667e51d58b8b3a3a971935a87f
mariajo228 <maria_jose.asprilla@uao.edu.co>
2025-10-27 19:41:08 -0500

    ajuste de escena
    ajuste mi escena, añadi el assets, ajuste el ambiente.
    ademas se ajusto la escena del menu

--- ProyectoFinal_CG/Assets/Game/Scripts/LoaderScene.cs
+++ ProyectoFinal_CG/Assets/Game/Scripts/LoaderScene.cs
@@ -3,20 +3,45 @@ using UnityEngine.SceneManagement;
 
 public class LoaderScene : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Paneles del menú")]
+    public GameObject panelMenuPrincipal;
+    public GameObject panelInstrucciones;
+    public GameObject panelControles;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Método para cambiar de escena
     public void LoaderScenes(string nameScene)
     {
         SceneManager.LoadScene(nameScene);
+    }
+
+    // Activar el panel de instrucciones
+    public void MostrarInstrucciones()
+    {
+        panelMenuPrincipal.SetActive(false);
+        panelInstrucciones.SetActive(true);
+        panelControles.SetActive(false);
+    }
+
+    // Activar el panel de controles
+    public void MostrarControles()
+    {
+        panelMenuPrincipal.SetActive(false);
+        panelInstrucciones.SetActive(false);
+        panelControles.SetActive(true);
+    }
+
+    // Volver al menú principal
+    public void VolverAlMenu()
+    {
+        panelInstrucciones.SetActive(false);
+        panelControles.SetActive(false);
+        panelMenuPrincipal.SetActive(true);
+    }
+
+    // Salir del juego (solo funciona en compilado)
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Saliendo del juego...");
     }
 }
