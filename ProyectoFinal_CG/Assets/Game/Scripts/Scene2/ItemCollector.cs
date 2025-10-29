f112da8a8331cafcfa17fe62088d57d64fb11f21
mariajo228 <maria_jose.asprilla@uao.edu.co>
2025-10-29 18:00:39 -0500

    Actualización escena2
    Agregue las animaciones de Eve (personaje principal).
    se descargo el enemigo.
    se agregaron los diamantes a la escena, también quedaron con su respectivo collider y cada vez que el personaje colisiona con el diamante se recoge y se suma en el panel.
    se corrigió que el player no atravesara las montañas.
    se organizo la cámara para que persiga el player.
    agregue un sonido de viento.

---
+++ ProyectoFinal_CG/Assets/Game/Scripts/Scene2/ItemCollector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TMPro;  
+
+public class ItemCollector : MonoBehaviour
+{
+    private int score = 0;
+    private int items = 0;
+
+    [Header("UI")]
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI itemText;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            score += 10;
+            items += 1;
+            UpdateUI();
+            Destroy(other.gameObject);
+        }
+    }
+
+    void UpdateUI()
+    {
+        if (scoreText != null)
+            scoreText.text = score.ToString("00");
+
+        if (itemText != null)
+            itemText.text = items.ToString("00");
+    }
+}
